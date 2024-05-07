36f94cfde6d1a2520c155083cb5fbcdc279b024c
Leo CorGuz <cleonardo21@gmail.com>
2024-05-07 14:24:40 -0600

    Correcion postdeploy script

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const pool = new Pool({
 */
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://mc_evs:mc_evs@localhost:5432/demo_mc_evs_test';
-const connectionStringSSL = process.env.DATABASE_URL ? '?ssl=false' : '?ssl=false';
+const connectionStringSSL = process.env.DATABASE_URL ? '?ssl=true&sslmode=require' : '?ssl=false';
 console.log('connectionString',connectionString+connectionStringSSL);
 const client = new pg.Client(connectionString);
 
